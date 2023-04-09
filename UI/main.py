f8267872d2bd7834f912befc3f150251264bd5c7
Alexey Alexandrov <AleksandrovAlexey0929@yandex.ru>
2023-04-09 17:41:48 +0300

    add root's calculation

--- UI/main.py
+++ UI/main.py
@@ -1,4 +1,6 @@
 from Math.TransferProcess import draw_transfer_process
+from Math.Roots import draw_root_picture
 
 if __name__ == "__main__":
-    draw_transfer_process([2, 0, 0], [1, 0, 0], 10)
+    # draw_transfer_process([2, 0, 0], [1, 1, 1], 10)
+    draw_root_picture([1, 2, 1], [1, 0, 1])
